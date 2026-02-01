f09ca5c56e89625ba3d892d30d643e640e46f0d6
Tellis Ellis <tbellisiv@gmail.com>
2026-02-01 10:17:36 -0600

    Checkpoint

---
+++ test-projects/routing-dev/.sb/sandboxes/sb04/volumes/sb-init/init.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+NVM_VERSION=v0.40.3
+
+NODE_VERSION=v24.12.0
+
+curl -s -o- https://raw.githubusercontent.com/nvm-sh/nvm/$NVM_VERSION/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm install $NODE_VERSION
+
+cat <<EOF >>$HOME/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm use $NODE_VERSION
+EOF
+
+# corepack install
+npm install -g corepack
+
+#-- pnpm install (packageManager in package.json)
+corepack enable
+
+#NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm use $NODE_VERSION
